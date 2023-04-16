d96050773d1131b2dbf35623a55eee95737a6000
Jayoti700 <91897660+Jayoti700@users.noreply.github.com>
2023-04-16 21:37:41 +0530

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @FXML
+    private TextField urlTextField;
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("downloadManager"), 640, 480);
+        stage.setTitle("Jdownload Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+
+    }
+
+}
---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,7 @@
+package org.example.config;
+
+public class AppConfig
+{
+    public static final String DOWNLOAD_PATH="C:\\Java Projects\\JavaDownloader\\JavaDownloader\\Download";
+
+}
